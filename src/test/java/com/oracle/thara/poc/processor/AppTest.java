69f824b47bf673a5adefa1a076c0e2a0391fed2d
Takahiro Harashima <tharashi@Mac.local>
2022-04-15 19:15:09 +0900

    Initial Commit

---
+++ src/test/java/com/oracle/thara/poc/processor/AppTest.java
@@ -0,0 +1,21 @@
+package com.oracle.thara.poc.processor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+@Slf4j
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertEquals( "Oracle", "Oracle", "Test");
+    }
+}
